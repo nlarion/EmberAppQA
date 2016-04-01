3ffc0fd32eb378f62d199d5d7d7b057d1a893427
Neil Larion <neil.larion@gmail.com>
2016-04-01 10:42:46 -0700

    get basic questions displayig on the site and add answer model

--- app/models/question.js
+++ app/models/question.js
@@ -1,5 +1,12 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  author: DS.attr(),
+  date: DS.attr(),
+  downVotes: DS.attr(),
+  notes: DS.attr(),
+  question: DS.attr(),
+  tag: DS.attr(),
+  upVotes: DS.attr(),
+  answers: DS.hasMany('answer', {async: true})
 });
